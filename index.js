abc0e3d0947b8ff6ab1d45fdf9fe27f7b9f459d6
delta <nishchal98765@gmail.com>
2022-07-17 14:34:38 +0530

    test1

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function connect() {
     mongo.connect("mongodb+srv://messenger:Project%40123@cluster0.10fn5ry.mongodb.net/?retryWrites=true&w=majority").then(console.log("Sucessfully ! connected to database")).catch(err => console.log(err))
 }
 app.get('/', (req, res) => res.send("helllloooo"))
+app.get("/sayhi", (req, res) => res.send(" hi"))
 const schma = new mongo.Schema({
     name: String
 })
