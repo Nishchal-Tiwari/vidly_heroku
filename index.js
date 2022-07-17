8711d9b5d69aae4b38faa14d1875f04496db86a5
delta <nishchal98765@gmail.com>
2022-07-17 14:24:35 +0530

    test1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const mongo = require('mongoose');
+const express = require('express')
+const app = express();
 
 function connect() {
     mongo.connect("mongodb+srv://messenger:Project%40123@cluster0.10fn5ry.mongodb.net/?retryWrites=true&w=majority").then(console.log("Sucessfully ! connected to database")).catch(err => console.log(err))
 }
-
+app.get('/', (req, res) => res.send("helllloooo"))
 const schma = new mongo.Schema({
     name: String
 })
@@ -11,3 +13,4 @@ const rr = mongo.model('vids', schma)
 const s = new rr({ name: "roorkee" })
 s.save()
 connect()
+app.listen(process.env.PORT || '3000', () => console.log("started"))
