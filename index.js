77cddbab568ed2a0584c5ba6c046a7a4d8e02f58
delta <nishchal98765@gmail.com>
2022-07-17 18:36:07 +0530

    test1

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ function connect() {
 }
 app.get('/', (req, res) => res.send("helllloooo"))
 app.get("/sayhi", (req, res) => res.send(" hi"))
-app.get('/ask', (req, res) => {
-    res.send("hello ! " + req.query.name + " Your query has been recorded")
-})
+
 const schma = new mongo.Schema({
     name: String
 })
 const rr = mongo.model('vids', schma)
-const s = new rr({ name: "roorkee" })
-s.save()
+app.get('/ask', (req, res) => {
+    const s = new rr({ name: req.query.name })
+    s.save()
+    res.send("hello ! " + req.query.name + " Your query has been recorded")
+})
+
 connect()
 app.listen(process.env.PORT || '3000', () => console.log("started"))
